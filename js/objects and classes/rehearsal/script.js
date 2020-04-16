adbc67b029ecff8067bb4eb3fcd38b0b0a6c288b
Tal Yaron <tal.yaron@gmail.com>
2020-04-16 19:47:52 +0300

    fix

--- js/objects and classes/rehearsal/script.js
+++ js/objects and classes/rehearsal/script.js
@@ -1,8 +1,18 @@
 const message = document.getElementById("textbox");
 
 const btnSend = document.getElementById("btnSend");
+const myMessages = document.getElementById('myMessages');
+
 console.dir(btnSend)
 btnSend.addEventListener("click", e => {
-console.log(message.value)
+    console.log(message.value);
 
+    //add a child
+    var node = document.createElement("p");                 // Create a <li> node
+    var textnode = document.createTextNode(message.value);         // Create a text node
+    node.appendChild(textnode);                              // Append the text to <p>
+    myMessages.appendChild(node);     // Append <li> to <ul> with id="myList" 
+
+
+    message.value = '';
 });
