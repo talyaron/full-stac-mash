6a09638eaa227a32ac0df0fe516ace6d6d81e2df
Tal Yaron <tal.yaron@gmail.com>
2020-05-21 18:53:27 +0300

    tal

--- node/express/CRUD/crud homework/public/functions.js
+++ node/express/CRUD/crud homework/public/functions.js
@@ -1,90 +1,97 @@
-fetch('/api/get-products')
-.then(responce => responce.json())
-.then(res2 => {
-    
-    console.log(res2)
 
-    let productsSTR = '';
-    res2.forEach(product => {
-        productsSTR += `<p>Type: ${product.type} ProductId: ${product.productId} Name: ${product.name} Price: ${product.price}$ <img src="img/${product.img}" alt="some lipstick" > </p>`
-    })
+function getAllProducts() {
+    fetch('/api/get-products')
+        .then(responce => responce.json())
+        .then(res2 => {
 
-    document.getElementById('products').innerHTML = productsSTR;
-});
+            console.log(res2)
 
-fetch('/api/get-lipsticks')
-.then(responce => responce.json())
-.then(res2 => {
-    
-    console.log(res2)
+            let productsSTR = '';
+            res2.forEach(product => {
+                productsSTR += `<p>Type: ${product.type} ProductId: ${product.productId} Name: ${product.name} Price: ${product.price}$ <img src="img/${product.img}" alt="some lipstick" > </p>`
+            })
 
-    let productsSTR = '';
-    res2.forEach(product => {
-        productsSTR += `<p>Type: ${product.type} ProductId: ${product.productId} Name: ${product.name} Price: ${product.price}$ <img src="img/${product.img}" alt="some lipstick" > </p>`
-    })
+            document.getElementById('products').innerHTML = productsSTR;
+        });
+}
 
-    document.getElementById('lipsticks').innerHTML = productsSTR;
-});
+function getLipsticks() {
 
-fetch('/api/get-foundations')
-.then(responce1 => responce1.json())
-.then(res3 => {
-    
-    console.log(res3)
+    fetch('/api/get-lipsticks')
+        .then(responce => responce.json())
+        .then(res2 => {
 
-    let productsSTR1 = '';
-    res3.forEach(product => {
-        productsSTR1 += `<p>Type: ${product.type} ProductId: ${product.productId} Name: ${product.name} Price: ${product.price}$ <img src="img/${product.img}" alt="some lipstick" > </p>`
-    })
+            console.log(res2)
 
-    document.getElementById('foundations').innerHTML = productsSTR1;
-});
+            let productsSTR = '';
+            res2.forEach(product => {
+                productsSTR += `<p>Type: ${product.type} ProductId: ${product.productId} Name: ${product.name} Price: ${product.price}$ <img src="img/${product.img}" alt="some lipstick" > </p>`
+            })
+
+            document.getElementById('lipsticks').innerHTML = productsSTR;
+        });
+}
+function getAllFoundation() {
+    fetch('/api/get-foundations')
+        .then(responce1 => responce1.json())
+        .then(res3 => {
+
+            console.log(res3)
+
+            let productsSTR1 = '';
+            res3.forEach(product => {
+                productsSTR1 += `<p>Type: ${product.type} ProductId: ${product.productId} Name: ${product.name} Price: ${product.price}$ <img src="img/${product.img}" alt="some lipstick" > </p>`
+            })
+
+            document.getElementById('foundations').innerHTML = productsSTR1;
+        });
+}
 
 fetch('/api/get-eyeshadows')
-.then(responce2 => responce2.json())
-.then(res4 => {
-    
-    console.log(res4)
+    .then(responce2 => responce2.json())
+    .then(res4 => {
 
-    let productsSTR2 = '';
-    res4.forEach(product => {
-        productsSTR2 += `<p>Type: ${product.type} ProductId: ${product.productId} Name: ${product.name} Price: ${product.price}$ <img src="img/${product.img}" alt="some lipstick" > </p>`
-    })
+        console.log(res4)
 
-    document.getElementById('eyeshadows').innerHTML = productsSTR2;
-});
+        let productsSTR2 = '';
+        res4.forEach(product => {
+            productsSTR2 += `<p>Type: ${product.type} ProductId: ${product.productId} Name: ${product.name} Price: ${product.price}$ <img src="img/${product.img}" alt="some lipstick" > </p>`
+        })
+
+        document.getElementById('eyeshadows').innerHTML = productsSTR2;
+    });
 
 function addProduct(e) {
-e.preventDefault();
+    e.preventDefault();
 
-console.log(e)
+    console.log(e)
 
-let {type, productId, img, name, price } = e.target.elements;
-type = type.value;
-productId = productId.value;
-img = img.value;
-name = name.value;
-price = price.value;
-console.log(type, productId, img, name, price);
+    let { type, productId, img, name, price } = e.target.elements;
+    type = type.value;
+    productId = productId.value;
+    img = img.value;
+    name = name.value;
+    price = price.value;
+    console.log(type, productId, img, name, price);
 
-fetch('/api/add-product', {
-    method: 'POST',
-    headers: {
-        'Accept': 'application/json',
-        'Content-Type': 'application/json'
-    },
-    body: JSON.stringify({type, productId, img, name, price})
-})
-.then(responce => responce.json())
-.then(res2 => {
-    console.log('first line')
-    console.log(res2)
-
-    let producrtsSTR = '';
-    res2.forEach(product => {
-        producrtsSTR += `<p>Type: ${product.type} ProductId: ${product.productId} <img src="img/${product.img}" alt="some lipstick" >  Name: ${product.name} Price: $${product.price}</p>`
+    fetch('/api/add-product', {
+        method: 'POST',
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ type, productId, img, name, price })
     })
+        .then(responce => responce.json())
+        .then(res2 => {
+            console.log('first line')
+            console.log(res2)
 
-    document.getElementById('products').innerHTML = producrtsSTR;
-});
+            let producrtsSTR = '';
+            res2.forEach(product => {
+                producrtsSTR += `<p>Type: ${product.type} ProductId: ${product.productId} <img src="img/${product.img}" alt="some lipstick" >  Name: ${product.name} Price: $${product.price}</p>`
+            })
+
+            document.getElementById('products').innerHTML = producrtsSTR;
+        });
 }
