3e091967e4b70ecb7d4387f059dddbf65d7384cf
Tal Yaron <tal.yaron@gmail.com>
2020-06-03 20:39:05 +0300

    react cont

--- React/basic/basic-app/src/App.js
+++ React/basic/basic-app/src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import logo from './logo.svg';
+import flower from './img/flower.jpg';
 import './App.css';
 
 function App() {
@@ -7,17 +8,18 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <img src={flower} alt='flower' className='img' />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
            Hi Masha, Learn React
-        <DailyMenuItem />
-        <DailyMenuItem />
-        <DailyMenuItem />
-        <DailyMenuItem />
-        <DailyMenuItem />
-        <DailyMenuItem />
-        <DailyMenuItem />
+        <DailyMenuItem name={'Avocado'} clalories={12}/>
+        <DailyMenuItem name={'Tomato'} clalories={12}/>
+        <DailyMenuItem name={'Olive oil'} clalories={12}/>
+        <DailyMenuItem name={'Tuna'} clalories={12}/>
+        <DailyMenuItem name={'Mac'} clalories={34}/>
+        <DailyMenuItem name={'Salad'} clalories={345}/>
+        <DailyMenuItem name={'Mionize'} clalories={34}/>
         <Food />
       </header>
     </div>
@@ -25,10 +27,18 @@ function App() {
 }
 
 // a component
-function DailyMenuItem() {
+function DailyMenuItem(props) {
+  const {name, clalories} = props;
+
   return (
-    <p>Hi, I am a daily menu item</p>
+  <p>Hi, I am a daily menu item: {name}, and I have {clalories} calories <Edit name={name}/></p>
   )
+}
+
+function Edit(props){
+  const {name} = props;
+
+  return(<button onClick={()=>{alert(name)}}>ADD {name}</button>)
 }
 
 // another comnponent
