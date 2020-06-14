533efee6ccf60e4b137aa58a359a909c43fe1bb4
Masha <masha.alarcon@gmail.com>
2020-06-14 17:50:11 +0300

    madOrBad homework

--- React/basic/madOrBad/src/App.js
+++ React/basic/madOrBad/src/App.js
@@ -2,12 +2,20 @@ import React, { useState } from 'react';
 import './App.css';
 import bomb from './img/bomb.png';
 import explosion from './img/explosion.png';
+import serious from './img/serious.jpg';
+import laughing from './img/laughing.jpg';
 
 //create 16 img
 let imgs = []
-for(let i=0;i<16;i++){
+for(let i=0;i<18;i++){
+  let m = Math.random();
+  if (m > 0.5){
   imgs.push(bomb)
+  } else {imgs.push(serious)}
 }
+
+
+
 
 
 
@@ -18,7 +26,11 @@ function App() {
     <div className="app">
       {
         imgs.map((img,index)=>{
+          if(img == bomb){
           return(<Bomb key={index} img={img}/>)
+          } else {
+            return(<Serious key={index} img={img}/>)
+          }
         })
       }
     </div>
@@ -30,9 +42,18 @@ function App() {
 function Bomb(props){
   const {img} = props;
   const [isBomb, setIsBomb] = useState(true);
+  
 
 
   return(<img src={isBomb?img:explosion} onClick={()=>{ setIsBomb(!isBomb)}}/>)
+}
+
+function Serious(props){
+  const {img} = props;
+  const [isSerious, setIsSerious] = useState(true);
+
+  return(<img src={isSerious?img:laughing} onClick={()=>{ setIsSerious(!isSerious)}}/>)
+
 }
 
 
