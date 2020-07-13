a7e07c9ba91c25412a3fec56054408da02ddfc05
Tal Yaron <tal.yaron@gmail.com>
2020-07-13 20:29:51 +0300

    bem

---
+++ projects/sales/socialsales/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.scss';
+
+import Card from './components/Card/Card';
+
+function App() {
+  return (
+    <div className="App">
+      <Card />
+    </div>
+  );
+}
+
+export default App;
---
+++ projects/sales/socialsales/src/components/Card/Card.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import './Card.scss';
+
+const Card = () => {
+    return (
+        <div className='card'>
+            <div className='card__image'>
+                img
+            </div>
+            <div className='card__info-box'>
+                <div className='card__title-box'>
+                    <div className='card__title'>
+                        sdfsdf
+                    </div>
+                    <div className='card__price'>
+sdgsdg
+                    </div>
+                </div>
+                <div className='card__details-box'>
+                    <div className='card__details-line'>
+                        sdg
+                    </div>
+                    <div className='card__details-line'>
+
+                        sdg
+                    </div>
+                    <div className='card__details-line--discount'>
+
+                        sdg
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+export default Card
