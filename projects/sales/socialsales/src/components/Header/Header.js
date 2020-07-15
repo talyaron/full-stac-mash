d3c613641466d406bc24fd8fb6f221399d382ca8
Masha Alarcon <masha.alarcon@gmail.com>
2020-07-15 20:13:42 +0300

    cont 3

--- projects/sales/socialsales/src/components/Header/Header.js
+++ projects/sales/socialsales/src/components/Header/Header.js
@@ -6,34 +6,34 @@ import Logo from '../../img/logo.svg'
 const Header = () => {
     return (
         <div className='header'>
-        <div className='header__box'>
-            <div className='header__logo'>
-                <img src={Logo} alt='logo'/>
-                <div>Social sales</div>
+            <div className='header__box'>
+                <div className='header__logo'>
+                    <img src={Logo} alt='logo' />
+                    <div>Social sales</div>
+                </div>
+                <div className='header__sub-title'>So Far You Earned</div>
+                <div className='header__money'>$127</div>
             </div>
-            <div className='header__sub-title'>So Far You Earned</div>
-            <div className='header__money'>$127</div>
-       </div>
-       
 
-       <div className='header__box--small'>
-           <div className='header__box--tiny'>
-           <div className='header__subtitle--small'>Small Subtitle</div>
-            <div className='header__money--small'>Small $$</div>
-       </div>
-          <div className='header__box--tiny'>
-           <div className='header__subtitle--small'>Small Subtitle</div>
-           <div className='header__money--small'>Small $$</div>
+
+            <div className='header__box--small'>
+                <div className='header__box--tiny'>
+                    <div className='header__sub-title--small'>This month you earned</div>
+                    <div className='header__money--small'>$50</div>
+                </div>
+                <div className='header__box--tiny'>
+                    <div className='header__sub-title--small'>This week you earned </div>
+                    <div className='header__money--small'>$14</div>
+                </div>
+
+
+            </div>
         </div>
 
 
-       </div>
-       </div>
-            
 
 
-        
-            
+
     )
 }
 export default Header
